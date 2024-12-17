47cda5579978cb46749e6658ab311d3f528264e3
nkriventsov <61682500+nkriventsov@users.noreply.github.com>
2024-12-17 10:14:06 +0300

    FX rate update

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from src.api.v1 import router as v1_router
 from src.api import healthcheck
-from src.api import tasks
+from src.api.v1.endpoints import fx_rate_upd
 
 api_router = APIRouter()
 api_router.include_router(v1_router)
 api_router.include_router(healthcheck.healthcheck_router)
 
-api_router.include_router(tasks.tasks_router)
+api_router.include_router(fx_rate_upd.fx_rate)
 
